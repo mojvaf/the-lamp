86c8a343bf0a8f3260f577fef55988a8ceed2421
mojvaf <mvafa44@gmail.com>
2020-12-10 04:55:57 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+<div id='app'>
+ <div class='lamp' v-on:click='toggle'>
+   <div class='shade'>
+     <div class='light' v-if='on'>
+     </div>
+   </div>
+   <div class='base'></div>
+ </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+   el: '#app',
+    methods:{
+     toggle: function(){
+      this.on = !this.on
+     }
+   }
+}
+</script>
